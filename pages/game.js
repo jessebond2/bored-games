31657d04672c590eb096c61ee2eae731c38539d7
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2020-04-26 14:31:27 -0700

    Initial setup

---
+++ pages/game.js
@@ -0,0 +1,19 @@
+function GamePage() {
+    return (
+        <div>
+            <h1>Gamepage</h1>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="./game">Game</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default GamePage
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to boredgames.js!</h1>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="./game">Game</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
